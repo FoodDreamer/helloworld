cad85d78577a492a91f36b49a95bd8126cddd38a
Pythonroot2021@gmail.com <Ant.lytvinenko@gmail.com>
2021-11-18 10:30:44 +0200

    add file

---
+++ Lesson 2.py
@@ -0,0 +1,16 @@
+# greetings
+print("Hi user, my name is Zed")
+# username input
+name = input("What you name?")
+# response to user
+print("nice friend")
+# first question to the user
+a = input("guess the first number")
+# response to user
+print("well done")
+# second question to the user
+b = input("guess the second number")
+# response to user
+print("ok my friend the sum of these numbers")
+# sum of numbers
+print(int(a) + int(b))
